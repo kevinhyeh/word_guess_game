243870aebdd857f2a8dba404235ec3e1a89ab082
Kevin Yeh <kevinyeh@Kevins-MacBook-Pro-3.local>
2018-06-06 00:43:18 -0700

    added theme style and begun javascript for current word

---
+++ assets/javascript/game.js
@@ -0,0 +1,12 @@
+var anthem = ["United States Of America", "Yemen", "Zimbabwe", "Canada", "Bangladesh", "Egypt", "Iraq", "Mexico", "Switzerland"];
+
+// document.querySelector('.hidden').innerHTML = anthem[1];
+
+var song = anthem[0];
+var dash = "-";
+
+for (i = 0; i < song.length; i++){
+	i = "-"
+	document.querySelector(".hidden").innerHTML = dash += i + " ";
+	// console.log(dash += i + " ");
+}
