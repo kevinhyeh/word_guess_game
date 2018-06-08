a93b02651ef70cebfe84ebf0693ec046ab0f89ff
Kevin Yeh <kevinyeh@Kevins-MacBook-Pro-3.local>
2018-06-07 18:42:21 -0700

    displayed necessary numbers/letters in their respective areas

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,11 +1,41 @@
-var anthem = ["United States Of America", "Yemen", "Zimbabwe", "Canada", "Bangladesh", "Egypt", "Iraq", "Mexico", "Switzerland"];
+var words = ["USA", "Yemen", "Zimbabwe", "Canada", "Bangladesh", "Egypt", "Iraq", "Mexico", "Switzerland"];
 
-// document.querySelector('.hidden').innerHTML = anthem[1];
+var randomIndex = Math.floor(Math.random() * words.length);
+var song = words[randomIndex];
+// var dash = "-".repeat(song.length);
 
-var randomIndex = Math.floor(Math.random()*anthem.length)
-var song = anthem[randomIndex];
-var dash = "-".repeat(song.length);
+var letters = [];
+var num = 15;
+var numWins = 0;
+// document.querySelector('.hidden').innerHTML = words[1];
 
-	document.querySelector(".hidden").innerHTML = dash;
+document.querySelector(".counter").innerHTML = num;
+document.querySelector(".wins").innerHTML = numWins;
+document.querySelector(".hidden").innerHTML = song;
 
-// console.log(randomIndex);
+
+function hangman(e) {
+    var key = e.key;
+    // current word
+    if (key == "r") {
+        document.querySelector(".hidden").innerHTML = document.querySelector(".hidden").innerHTML.replace(randomIndex);
+        console.log(randomIndex);
+    }
+
+    // number of wins
+    document.querySelector(".wins").innerHTML = numWins++;
+
+    // letters guessed
+    letters.push(" " + key);
+
+    document.querySelector(".guesses").innerHTML = letters;
+
+    if (num == 0) {
+        document.querySelector(".guesses").innerHTML = letters.splice(0, letters.length);
+    }
+
+    // number of guesses left
+    document.querySelector(".counter").innerHTML = num--;
+}
+
+document.onkeypress = hangman;
