d53075f83bfc3fabee88a8f0a0d93b4f60624c48
Kevin Yeh <kevinyeh@Kevins-MacBook-Pro-3.local>
2018-06-09 14:13:33 -0700

    added images, made guesses left counter only go down when letter is incorrect, show word if out of guesses

--- assets/javascript/game1.js
+++ assets/javascript/game1.js
@@ -1,7 +1,8 @@
 var words = ["USA", "Yemen", "Zimbabwe", "Japan", "Bangladesh", "Egypt", "Iraq", "Mexico", "Switzerland"];
 var randomIndex = Math.floor(Math.random() * words.length);
 var pickedWord = words[randomIndex];
-
+var lowerLetter = pickedWord.toLowerCase();
+var correctLetter = lowerLetter.split("");
 
 // answer array
 var answerArray = [];
@@ -9,34 +10,32 @@ for (var i = 0; i < pickedWord.length; i++) {
     answerArray[i] = "-";
 }
 
-
-
 var letters = [];
 var num = 15;
-var wins = 0;
+var wins = parseInt(0);
 
 document.querySelector(".counter").innerHTML = num;
 document.querySelector(".wins").innerHTML = wins;
 document.querySelector(".hidden").innerHTML = answerArray.join("");
 
-
-
 function hangman(e) {
     var key = e.key;
-    var remainingLetters = pickedWord.length;
-    var correctLetter = pickedWord.toLowerCase();
-    num--;
+    var rCharacters = pickedWord.length;
+    lowerLetter = pickedWord.toLowerCase();
 
-    for (var a = 0; a < remainingLetters; a++) {
+    if (answerArray.join("") == lowerLetter) {
+        repeat();
+    }
+
+    for (var a = 0; a < rCharacters; a++) {
 
         document.querySelector(".hidden").innerHTML = answerArray.join("");
 
-        for (var c = 0; c < remainingLetters; c++) {
-            if (key == correctLetter[c]) {
+        for (var c = 0; c < rCharacters; c++) {
+            if (key == lowerLetter[c]) {
                 answerArray[c] = key;
-                remainingLetters--;
+                rCharacters--;
             }
-
         }
     }
 
@@ -52,41 +51,69 @@ function hangman(e) {
 
     }
 
-    if (num == 0) {
-        repeat();
-        document.querySelector(".picture").innerHTML = "You Lost";
-    } else if (num > 0) {
-    	document.querySelector(".picture").innerHTML = "";
-    }
+  
 
-    // letters guessed
-    letters.push(" " + key);
-
-    document.querySelector(".guesses").innerHTML = letters;
-
-    if (num == 0) {
-        letters.splice(0, letters.length);
-        document.querySelector(".guesses").innerHTML = letters;
-    }
+    // document.querySelector(".guesses").innerHTML = letters;
 
 
 
 
-
-
-    // number of guesses left
-    document.querySelector(".counter").innerHTML = num;
-
-    if (num == 0) {
-        num = 15;
+    // counter
+    if (correctLetter.includes(key) == false) {
+        num--;
         document.querySelector(".counter").innerHTML = num;
     }
 
 
-    if (answerArray.join("") == pickedWord.toLowerCase()) {
-        document.querySelector(".wins").innerHTML = wins++;
-    } 
+    // if (num == 0) {
+
+    // }
+
+    if (num == 0) {
+        document.querySelector(".hidden").innerHTML = pickedWord;
+        document.querySelector(".picture").innerHTML = "You Lost!" + "<br>" + "Press 'Enter' to play again";
+        letters.splice(0, letters.length);
+        document.querySelector(".guesses").innerHTML = letters;
+        num = 15;
+        document.querySelector(".counter").innerHTML = num;
+    }
+
+    // if (num == 0) {
+
+    // }
+
+    // reset
+    if (key == "Enter") {
+        repeat();
+        document.querySelector(".picture").innerHTML = "";
+        num = 15;
+        document.querySelector(".counter").innerHTML = num;
+        letters.splice(0, letters.length);
+        document.querySelector(".guesses").innerHTML = letters;
+    }
+
+      // letters guessed
+
+    if (e.keyCode == 32 || key == "Enter" || key == "[" || key == "]") {
+        letters;
+    } else {
+        letters.push(" " + key);
+    }
+
+    if (correctLetter.includes(key) == false) {
+        letters.push(" " + key)
+        document.querySelector(".guesses").innerHTML = key;
+    }
+    // you won text
+    if (answerArray.join("") == lowerLetter) {
+        document.querySelector(".picture").innerHTML = "You Won! Press 'Enter' to keep playing";
+        wins++;
+        document.querySelector(".wins").innerHTML = wins;
+    }
+
 }
+
+
 
 
 document.onkeypress = hangman;
