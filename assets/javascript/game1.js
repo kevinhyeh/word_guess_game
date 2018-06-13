874798673858637dc4d410608bb55a8ba16b2e15
Kevin Yeh <kevinyeh@Kevins-MacBook-Pro-3.local>
2018-06-13 00:41:56 -0700

    added bonus feature, song and picture

--- assets/javascript/game1.js
+++ assets/javascript/game1.js
@@ -15,8 +15,19 @@ var letters = [];
 var num = 15;
 var wins = parseInt(0);
 
+// audio variables
+var japanAudio = new Audio('assets/audio/japan_anthem.mp3');
+var usaAudio = new Audio('assets/audio/usa_anthem.mp3');
+var mexicoAudio = new Audio('assets/audio/mexico_anthem.mp3');
+var yemenAudio = new Audio('assets/audio/yemen_anthem.mp3');
+var zimbabweAudio = new Audio('assets/audio/zimbabwe_anthem.mp3');
+var bangladeshAudio = new Audio('assets/audio/bangladesh_anthem.mp3');
+var egyptAudio = new Audio('assets/audio/egypt_anthem.mp3');
+var iraqAudio = new Audio('assets/audio/iraq_anthem.mp3');
+var switzerlandAudio = new Audio('assets/audio/switzerland_anthem.mp3');
+
 // first page load
-document.querySelector(".picture").innerHTML = "Press any key to start";
+document.querySelector(".message").innerHTML = "Press any key to start";
 document.querySelector(".counter").innerHTML = num;
 document.querySelector(".wins").innerHTML = wins;
 document.querySelector(".hidden").innerHTML = answerArray.join("");
@@ -43,6 +54,7 @@ function hangman(e) {
         }
     }
 
+    // rechoose word function
     function repeat() {
         answerArray.splice(0, answerArray.length);
         randomIndex = Math.floor(Math.random() * words.length);
@@ -64,39 +76,89 @@ function hangman(e) {
         document.querySelector(".counter").innerHTML = num;
     }
 
-    // you lost message
+    // you lost message, reset letters guessed, reset num of guessed letters
     if (num == 0) {
         document.querySelector(".hidden").innerHTML = pickedWord;
-        document.querySelector(".picture").innerHTML = "You Lost" + "<br>" + "The word is " + pickedWord + "<br>" + "Press Enter to play again";
+        document.querySelector(".message").innerHTML = "You Lost." + "<br>" + "<br>" + "The word is " + pickedWord + "." + "<br>" + "<br>" + "Press Enter to play again.";
         letters.splice(0, letters.length);
         document.querySelector(".guesses").innerHTML = letters;
         num = 15;
         document.querySelector(".counter").innerHTML = num;
     }
 
-    // you won message
+    // you won message, increase wins by 1
     if (answerArray.join("") == lowerLetter) {
-        document.querySelector(".picture").innerHTML = "You Won!" + "<br>" + "Press Enter to play again";
+        document.querySelector(".message").innerHTML = "You Won! Turn up volume to hear anthem." + "<br>" + "Press Enter to play again.";
         wins++;
         document.querySelector(".wins").innerHTML = wins;
+    }
+
+    // audio play and pause functions
+    function audioPause(songPause) {
+        songPause.pause();
+    }
+
+    function audioPlay(songPlay) {
+        songPlay.play();
     }
 
     // reset
     if (key == "Enter") {
         repeat();
-        document.querySelector(".picture").innerHTML = "";
+        document.querySelector(".message").innerHTML = "";
         num = 15;
         document.querySelector(".counter").innerHTML = num;
         letters.splice(0, letters.length);
         document.querySelector(".guesses").innerHTML = letters;
+        document.querySelector(".picture").style.visibility = "hidden";
+        audioPause(japanAudio);
+        audioPause(bangladeshAudio);
+        audioPause(egyptAudio);
+        audioPause(iraqAudio);
+        audioPause(mexicoAudio);
+        audioPause(switzerlandAudio);
+        audioPause(usaAudio);
+        audioPause(yemenAudio);
+        audioPause(zimbabweAudio);
     }
 
+    // show flag
+    if (answerArray.join("") == lowerLetter && lowerLetter == "japan") {
+        audioPlay(japanAudio);
+        document.querySelector(".picturebox").innerHTML = '<img src="assets/images/japan_flag.png" class="picture">';
+    } else if (answerArray.join("") == lowerLetter && lowerLetter == "bangladesh") {
+        audioPlay(bangladeshAudio);
+        document.querySelector(".picturebox").innerHTML = '<img src="assets/images/bangladesh_flag.png" class="picture">';
+    } else if (answerArray.join("") == lowerLetter && lowerLetter == "egypt") {
+        audioPlay(egyptAudio);
+        document.querySelector(".picturebox").innerHTML = '<img src="assets/images/egypt_flag.jpg" class="picture">';
+    } else if (answerArray.join("") == lowerLetter && lowerLetter == "iraq") {
+        audioPlay(iraqAudio);
+        document.querySelector(".picturebox").innerHTML = '<img src="assets/images/iraq_flag.png" class="picture">';
+    } else if (answerArray.join("") == lowerLetter && lowerLetter == "mexico") {
+        audioPlay(mexicoAudio);
+        document.querySelector(".picturebox").innerHTML = '<img src="assets/images/mexico_flag.png" class="picture">';
+    } else if (answerArray.join("") == lowerLetter && lowerLetter == "switzerland") {
+        audioPlay(switzerlandAudio);
+        document.querySelector(".picturebox").innerHTML = '<img src="assets/images/switzerland_flag.png" class="picture">';
+    } else if (answerArray.join("") == lowerLetter && lowerLetter == "usa") {
+        audioPlay(usaAudio);
+        document.querySelector(".picturebox").innerHTML = '<img src="assets/images/usa_flag.png" class="picture">';
+    } else if (answerArray.join("") == lowerLetter && lowerLetter == "yemen") {
+        audioPlay(yemenAudio);
+        document.querySelector(".picturebox").innerHTML = '<img src="assets/images/yemen_flag.png" class="picture">';
+    } else if (answerArray.join("") == lowerLetter && lowerLetter == "zimbabwe") {
+        audioPlay(zimbabweAudio);
+        document.querySelector(".picturebox").innerHTML = '<img src="assets/images/zimbabwe_flag.png" class="picture">';
+    }
 }
 
 
 
 
 document.onkeypress = hangman;
+
+
 
 
 
